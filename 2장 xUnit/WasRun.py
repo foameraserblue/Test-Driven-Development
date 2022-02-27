246e63c71ffd557d7b95064b05cb84268641e96f
foameraserblue@gmail.com <foameraserblue@gmail.com>
2022-02-27 18:17:33 +0900

    18~19

---
+++ 2장 xUnit/WasRun.py
@@ -0,0 +1,16 @@
+from TestCase import TestCase
+
+
+class WasRun(TestCase):
+    def __init__(self, name):
+        self.wasSetUp = 1
+        self.wasRun = None
+        TestCase.__init__(self, name)
+
+    def testMethod(self):
+        self.wasRun = 1
+
+    def setUp(self):
+        pass
+
+
